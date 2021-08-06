f4772e7695a5015555d37c6cdc7fbc2b08653c56
JAGoodrich <jagoodri@usc.edu>
2021-08-06 16:05:29 -0700

    August 6 update analysis with PFAS

--- example_code_from_dom/999_3.0.0_workflow_xmwas EXAMPLE.R
+++
@@ -1,8 +0,0 @@
-# work flow for xMWAS
-
-
-# directories for file architecture
-source("!directories.R")
-
-# load formats for variables
-# source("1.0.1_example_xmwas.R") # takes long time, crashes R
